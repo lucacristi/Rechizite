91203ed64b280a64938bdb2e859c266636dea8c7
lucacristi <flaviuscristianluca@gmail.com>
2021-03-31 09:11:23 +0100

    xml file added

--- src/main/java/TestGhiozdan.java
+++ src/main/java/TestGhiozdan.java
@@ -1,10 +1,23 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class TestGhiozdan {
     public static void main(String[] args) {
-        List<Rechizita> rechizite = initializeazaRechizite();
+        TestGhiozdan testGhiozdan = new TestGhiozdan();
+        List<Rechizita> rechizite = testGhiozdan.readfile("Rechizite.xml");
+
         Ghiozdan ghiozdan = new Ghiozdan(rechizite);
 
         System.out.println("Nr total rechizite: " + ghiozdan.getNrRechizite());
@@ -15,34 +28,58 @@ public class TestGhiozdan {
         ghiozdan.listCaiet();
         ghiozdan.listManual();
 
-        System.out.println("\nUltima rechizita: " + ghiozdan.elementAt(ghiozdan.getNrRechizite()-1));
-        System.out.println("\nRechizita stearsa: " + ghiozdan.remove(ghiozdan.getNrRechizite()-1));
-        System.out.println("\nUltima rechizita: " + ghiozdan.elementAt(ghiozdan.getNrRechizite()-1));
-
-        ghiozdan.add(new Manual("atlas", Arrays.asList("Mihai","Iulia"), "Nord"));
+        ghiozdan.add(new Manual("atlas", Arrays.asList("Mihai", "Iulia"), "Nord"));
 
         System.out.println("Nr total rechizite: " + ghiozdan.getNrRechizite());
         System.out.println("Nr total caiete: " + ghiozdan.getNrCaiete());
         System.out.println("Nr total manuale: " + ghiozdan.getNrManuale());
+
+        ghiozdan.listItems();
     }
 
-    private static List<Rechizita> initializeazaRechizite() {
-        Manual abecedar = new Manual("abecedar", Arrays.asList("Ion","Ana","Vasile"), "Corint");
-        Manual culegere = new Manual("culegere", Arrays.asList("Crina","Mihaela","Sorin"), "ArtPress");
+    private List<Rechizita> readfile(String fileName) {
 
-        Caiet matematica = new Caiet("patratele", "A5", 48);
-        Caiet romana = new Caiet("linii", "dictando", 48);
-        Caiet desen = new Caiet("desen", "blank", 80);
+        List<Rechizita> returnList = new ArrayList<>();
+        try {
+            URL res = getClass().getClassLoader().getResource(fileName);
+            File inputFile = Paths.get(res.toURI()).toFile();
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
 
-        List<Rechizita> rechizite = new ArrayList<>();
+            NodeList listaRechizite = doc.getElementsByTagName("rechizita");
 
-        rechizite.add(abecedar);
-        rechizite.add(culegere);
+            for (int i = 0; i < listaRechizite.getLength(); i++) {
+                Node nNode = listaRechizite.item(i);
 
-        rechizite.add(matematica);
-        rechizite.add(romana);
-        rechizite.add(desen);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) nNode;
+                    String nume = element.getElementsByTagName("nume").item(0).getTextContent();
 
-        return rechizite;
+                    if (element.getAttribute("type").equals("manual")) {
+                        List<String> autori = new ArrayList<>();
+
+                        Element nodeAutori = (Element) element.getElementsByTagName("autori").item(0);
+                        NodeList valueAutori = nodeAutori.getElementsByTagName("value");
+
+                        for (int j = 0; j < valueAutori.getLength(); j++) {
+                            autori.add(valueAutori.item(j).getTextContent());
+                        }
+
+                        String editura = element.getElementsByTagName("editura").item(0).getTextContent();
+                        returnList.add(new Manual(nume, autori, editura));
+                    } else {
+                        String tip = element.getElementsByTagName("tip").item(0).getTextContent();
+                        int numarFile = Integer.parseInt(element.getElementsByTagName("numarFile").item(0).getTextContent());
+                        returnList.add(new Caiet(nume, tip, numarFile));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return returnList;
     }
 }
